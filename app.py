7231a707e8c336f1eca5dad7695ded7d2cd0f090
jeremyKisner <jkisner984@gmail.com>
2021-03-03 22:24:47 -0500

    playing with jinja

---
+++ app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import json
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+with open('data/data.json', 'r') as data_file:
+	data = data_file.read()
+
+books = []
+for book in json.loads(data):
+	books.append(book['name'])
+print(books)
+@app.route("/get")
+def get():
+    return render_template('index.html', books=books)
+
+if __name__ == "__main__":
+    app.run()
